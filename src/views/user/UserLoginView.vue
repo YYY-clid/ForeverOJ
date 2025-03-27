5b1cc7577f079c2eb4323071145f490fc0e383a7
YYY-clid <365535290@qq.com>
2025-03-27 15:35:19 +0800

    新增Register

--- src/views/user/UserLoginView.vue
+++ src/views/user/UserLoginView.vue
@@ -11,7 +11,7 @@
       <a-form-item field="userAccount" label="账号">
         <a-input v-model="form.userAccount" placeholder="请输入账号" />
       </a-form-item>
-      <a-form-item field="userPassword" tooltip="密码不少于 8 位" label="密码">
+      <a-form-item field="userPassword" tooltip="密码不少于 6 位" label="密码">
         <a-input-password
           v-model="form.userPassword"
           placeholder="请输入密码"
@@ -21,7 +21,7 @@
         <a-button type="primary" html-type="submit" style="width: 120px">
           登录
         </a-button>
-        <a-button type="primary" html-type="submit" style="width: 120px">
+        <a-button type="primary" html-type="submit" style="width: 120px; margin-left: 180px;" @click="register">
           注册
         </a-button>
       </a-form-item>
@@ -64,4 +64,11 @@ const handleSubmit = async () => {
     message.error("登陆失败，" + res.message);
   }
 };
+
+function register() {
+  router.push({
+    path: "/user/register",
+    replace: true,
+  });
+}
 </script>
